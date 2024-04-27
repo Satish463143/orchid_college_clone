79552c4f33d7963ea7e1123e472eeaf132804fdf
Satish463143 <166995533+Satish463143@users.noreply.github.com>
2024-04-27 12:04:45 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+    });
+  });
